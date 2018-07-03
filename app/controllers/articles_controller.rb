ad4daa222c87273942c25852e20331d646e9b60e
Pranjal Bharali <pranjalbharali@icloud.com>
2018-07-03 09:44:21 +0000

    table

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -40,8 +40,18 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
+  
+   def destroy
+    
+    @article = Article.find(params[:id])
+    @article.destroy
+    flash[:notice] = "successfully deleted"
+    redirect_to articles_path
+   end
+    
   private
    def article_params
      params.require(:article).permit(:title, :description)
    end
+   
 end
